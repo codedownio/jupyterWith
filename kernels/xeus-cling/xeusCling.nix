3a6f79455286e2051beef1755cc166f3828bcd42
Tom McLaughlin <pyro777@gmail.com>
2019-07-04 16:47:01 -0600

    Get on latest xeusCling

--- kernels/xeus-cling/xeusCling.nix
+++ kernels/xeus-cling/xeusCling.nix
@@ -10,6 +10,7 @@
 , clang
 , ncurses
 , zlib
+, openssl
 }:
 let
   xtl = stdenv.mkDerivation {
@@ -90,73 +91,24 @@ let
       '';
   };
 
-  cryptopp = stdenv.mkDerivation rec {
-      name = "crypto++-${version}";
-      majorVersion = "8.0";
-      version = "${majorVersion}.0";
-
-      src = fetchFromGitHub {
-        owner = "weidai11";
-        repo = "cryptopp";
-        rev = "CRYPTOPP_8_0_0";
-        sha256 = "135p1qqzrrvkkc33y8j328pp3b6grnwka9sps77pdidrqy333bws";
-      };
-
-      buildInputs = [gcc];
-
-      configurePhase = let
-        marchflags =
-          if stdenv.isi686 then "-march=i686" else
-          if stdenv.isx86_64 then "-march=nocona -mtune=generic" else
-          "";
-        in
-        ''
-          sed -i GNUmakefile \
-            -e 's|-march=native|${marchflags} -fPIC|g' \
-            -e '/^CXXFLAGS =/s|-g ||'
-        '';
-
-      enableParallelBuilding = true;
-
-      makeFlags = [ "PREFIX=$(out)" ];
-      buildFlags = [ "libcryptopp.so" "libcryptopp.pc" ];
-      installFlags = [ "LDCONF=true" ];
-
-      doCheck = true;
-      checkPhase = "LD_LIBRARY_PATH=`pwd` make test";
-
-      # prefer -fPIC and .so to .a; cryptotest.exe seems superfluous
-      postInstall = ''
-        ln -sf "$out"/lib/libcryptopp.so.${version} "$out"/lib/libcryptopp.so.${majorVersion}
-      '';
-
-      meta = with stdenv.lib; {
-        description = "Crypto++, a free C++ class library of cryptographic schemes";
-        homepage = http://cryptopp.com/;
-        license = licenses.boost;
-        platforms = platforms.all;
-        maintainers = [ ];
-      };
-    };
-
   xeus = stdenv.mkDerivation {
       name = "xeus";
 
       src = fetchFromGitHub {
           owner = "QuantStack";
           repo = "xeus";
-          rev = "e77c56c0f3f9bac55be253bc8cad79e24f28ade0";
-          sha256 = "0sk2g45jg09crdzx0kkbr6s5890z3ybfynwx2xbchrcvc9mgc3bd";
+          rev = "092cf4590aa6c5b6aa1956c4eaf2b6f77a98fa66";
+          sha256 = "0ph2nv0c6ghl3za2k1ydv8g3iyjxc5iy4ys1v4i2r5yi2w4wlf70";
         };
 
-        buildInputs = [ cmake zeromq cppzmq cryptopp nlohmannJson xtl pkgconfig libuuid clang ];
+        buildInputs = [ cmake zeromq cppzmq nlohmannJson openssl xtl pkgconfig libuuid clang ];
 
         configurePhase = ''
           export CC=${clang}/bin/cc
           export CXX=${clang}/bin/c++
           mkdir build
           cd build
-          cmake -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$out ..
+          cmake -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$out -DPKG_CONFIG_EXECUTABLE=${pkgconfig}/bin/pkg-config ..
         '';
       };
 
@@ -170,7 +122,7 @@ let
           sha256 = "0i48ywiz7sgvln106phs0syd00kly0sirr8ib2hlri32j5awph8v";
         };
 
-      buildInputs = [ cmake zeromq cppzmq xeus libuuid xtl pkgconfig cling pugixml cxxopts nlohmannJson clang ncurses zlib ];
+      buildInputs = [ cmake zeromq cppzmq xeus libuuid xtl pkgconfig cling pugixml cxxopts nlohmannJson clang ncurses zlib openssl ];
 
       configurePhase = ''
         export CC=${clang}/bin/cc
